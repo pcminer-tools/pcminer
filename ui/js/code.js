b614050d7fe6bae2410422901bf12dcf65458e4c
Manu Sridharan <msridhar@gmail.com>
2025-02-28 14:38:52 -0800

    Add Google Scholar and DBLP search links (#70)

--- ui/js/code.js
+++ ui/js/code.js
@@ -548,7 +548,12 @@ var pcminer = function(){
 		      $("#wordCloud").jQCloud(words);
 		      
 		      var text = "";
-		      
+
+		      // Google Scholar and DBLP search links
+		      text += "<br><b>search</b><br>";
+		      text += "<a href='https://scholar.google.com/scholar?q=author%3A\"" + encodeForUrl(list[i].author) + "\"' target=\"_blank\">Google Scholar</a><br>";
+		      text += "<a href='https://google.com/search?q=" + encodeForUrl(list[i].author + " dblp") + "\"' target=\"_blank\">DBLP (via Google)</a><br>";
+
 		      var arr = new Array();
 		      for (var j=0; j < list[i].publications.length; j++){
 		    	  arr[j] = list[i].publications[j]; 
@@ -595,7 +600,16 @@ var pcminer = function(){
 		  }
 	  }
 	};
-	
+
+	function encodeForUrl(input) {
+        // Decode HTML entities first
+        const doc = new DOMParser().parseFromString(input, "text/html");
+        const decoded = doc.documentElement.textContent;
+
+        // Encode for URL
+        return encodeURIComponent(decoded);
+    }
+
 	/**
 	 * produce HTML for publication in the appropriate color
 	 */
